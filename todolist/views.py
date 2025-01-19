ead4f33837bc9050481fd9f68269aa1a1b37cde3
diya <shresthadiya59@gmail.com>
2025-01-19 16:17:18 +0545

    templates

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
 # Create your views here.
+def home(request):
+   person = [
+   {"name": "Alice", "age": 25},
+    {"name": "Bob", "age": 60},
+    {"name": "Charlie", "age": 22},
+    {"name": "Diana", "age": 58},
+    {"name": "Ethan", "age": 35},
+   ]
+   context = {
+      'name':"Home Page",
+      'persons': person       }
+   return render(request,'home.html',context)
+
+def index(request):
+   return HttpResponse("This is index function.")
+
+def contact(request):
+   return render(request, 'contact.html')
+# contact, about...
